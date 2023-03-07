5cc04ffe671374897c43c316b08180f033772a21
Hari <harishsarma.v@gmail.com>
2023-03-07 20:00:37 +0530

    updating roboshop-infra

--- main.tf
+++ main.tf
@@ -1,10 +1,22 @@
 module "network" {
-  source = "github.com/harishsarmav/tf-module-vpc"
-  env = var.env
-  default_vpc_id = var.default_vpc_id
+  source                = "github.com/harishsarmav/tf-module-vpc"
+  env                   = var.env
+  default_vpc_id        = var.default_vpc_id
 
   for_each              = var.vpc
   cidr_block            = each.value.cidr_block
 }
 
+module "subnets" {
+  source                 = "github.com/harishsarmav/tf-module-subnets"
+  env                    = var.env
+  default_vpc_id         = var.default_vpc_id
+
+  vpc_id                 = module.vpc.vpc_id
+
+  for_each               = var.subnets
+  cidr_block             = each.value.cidr_block
+  availability_zone      = each.value.availability_zone
+  name                   = each.value.name
+}
 
