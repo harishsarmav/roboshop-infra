a548805aea7ac3f8067c8da73e2fe65cea0ce4fa
Hari <harishsarma.v@gmail.com>
2023-03-15 11:01:28 +0530

    updating roboshop-infra

--- main.tf
+++ main.tf
@@ -11,20 +11,20 @@ module "vpc" {
 
 }
 
-#module "subnets" {
-#  source                      = "github.com/harishsarmav/tf-module-subnets"
-#  env                         = var.env
-#  default_vpc_id              = var.default_vpc_id
-#
-#  for_each                    = var.subnets
-#  cidr_block                  = each.value.cidr_block
-#  availability_zone           = each.value.availability_zone
-#  name                        = each.value.name
-#  vpc_id                      = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
-#  vpc_peering_connection_id   = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
-#  internet_gw                 = lookup(each.value, "internet_gw", false)
-#  nat_gw                      = lookup(each.value, "nat_gw", false)
-#}
+module "subnets" {
+  source                      = "github.com/harishsarmav/tf-module-subnets"
+  env                         = var.env
+  default_vpc_id              = var.default_vpc_id
+
+  for_each                    = var.subnets
+  cidr_block                  = each.value.cidr_block
+  availability_zone           = each.value.availability_zone
+  name                        = each.value.name
+  vpc_id                      = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+  vpc_peering_connection_id   = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
+  internet_gw                 = lookup(each.value, "internet_gw", false)
+  nat_gw                      = lookup(each.value, "nat_gw", false)
+}
 
 output "out" {
   value = module.vpc
